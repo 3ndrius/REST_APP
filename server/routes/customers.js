5afc713e8a25195c263f202c4a28055e6bf12093
3ndrius <andrzejo1991@gmail.com>
2018-11-17 11:40:03 +0100

    Adding put and del method to server side

--- server/routes/customers.js
+++ server/routes/customers.js
@@ -55,5 +55,54 @@ module.exports = (server) => {
             return next(new errors.InternalError(err.message));
           }
         });
+      
+        //update customer
+        server.put(
+          '/customers/:id',
+          async (req, res, next) => {
+            // Check for JSON
+            if (!req.is('application/json')) {
+              return next(
+                new errors.InvalidContentError("Expects 'application/json'")
+              );
+            }
+      
+            try {
+              const customer = await Customer.findOneAndUpdate(
+                { _id: req.params.id },
+                req.body
+              );
+              res.send(200);
+              next();
+            } catch (err) {
+              return next(
+                new errors.ResourceNotFoundError(
+                  `There is no customer with the id of ${req.params.id}`
+                )
+              );
+            }
+          }
+        );
+
+        //delete customer
+        server.del(
+          '/customers/:id',
+          async (req, res, next) => {
+            try {
+              const customer = await Customer.findOneAndRemove({
+                _id: req.params.id
+              });
+              res.send(204);
+              next();
+            } catch (err) {
+              return next(
+                new errors.ResourceNotFoundError(
+                  `There is no customer with the id of ${req.params.id}`
+                )
+              );
+            }
+          }
+        );
+
 
 };
