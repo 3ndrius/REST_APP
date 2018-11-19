6f5744e3d7c12ac3fd5a42fccc2986f5e99e5515
3ndrius <andrzejo1991@gmail.com>
2018-11-19 13:15:16 +0100

    Adding auth JWT

--- server/index.js
+++ server/index.js
@@ -1,23 +1,15 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
-const corsMiddleware = require('restify-cors-middleware')
+const rjwt = require('restify-jwt-community');
 
 const server = restify.createServer();
-
-
-const cors = corsMiddleware({
-  preflightMaxAge: 5, //Optional
-  origins: ['http://localhost:3000', 'http://localhost:4000'],
-  allowHeaders: ['API-Token'],
-  exposeHeaders: ['API-Token-Expiry']
-})
-server.pre(cors.preflight)
-server.use(cors.actual)
 
 // Middleware
 server.use(restify.plugins.bodyParser());
 
+// Protect Routes
+// server.use(rjwt({ secret: config.JWT_SECRET }).unless({ path: ['/auth'] }));
 
 server.listen(config.PORT, () => {
   mongoose.set('useFindAndModify', false);
@@ -33,5 +25,6 @@ db.on('error', err => console.log(err));
 
 db.once('open', () => {
   require('./routes/customers')(server);
+  require('./routes/users')(server);
   console.log(`Server started on port ${config.PORT}`);
 });
